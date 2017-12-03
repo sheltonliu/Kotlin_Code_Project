6e35ebe2a52eba12eaa331ef2d4d9fafb5a4a537
sheltonliu <liushuo90@gmail.com>
2017-12-04 07:41:08 +0800

    add kt

---
+++ KotlinGrammar/lib/src/main/java/b_类和对象/Person.kt
@@ -0,0 +1,14 @@
+package b_类和对象
+
+/**
+ * Created by liushuo on 17/11/30.
+ */
+class Person {
+    var name:String = ""
+    var age: Int =0
+
+    constructor(name:String, age:Int){
+        this.name = name
+        this.age = age
+    }
+}
---
+++ KotlinGrammar/lib/src/main/java/b_类和对象/TestPerson.kt
@@ -0,0 +1,27 @@
+package b_类和对象
+
+/**
+ * Created by liushuo on 17/11/30.
+ */
+object TestPerson {
+
+    @JvmStatic fun main(args: Array<String>) {
+
+        val p1 = Person("a",11)
+        val p2 = Person("b",12)
+        val p3 = Person("c",13)
+
+        val test = listOf(p1,p2,p3)
+        val t2 = mutableListOf(test)
+        if (test == t2){
+            System.out.print("=====")
+        }else{
+            System.out.print("!!")
+        }
+
+        if (test.get(0).age > 0){
+
+        }
+    }
+
+}
